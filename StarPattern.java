18caf4f01c5089feaebe9b201b79215e0755cc2d
Shahariar Fahim <68606052+sfahim1996@users.noreply.github.com>
2025-06-11 04:22:49 -0400

    Add files via upload

---
+++ StarPattern.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package starpattern;
+
+/**
+ *
+ * @author Owner
+ */
+public class StarPattern {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+
+
+         System.out.println("     *");
+         System.out.println("    ***");
+         System.out.println("   *****");
+         System.out.println("  *******");
+         
+         
+         //Print the Bottom half of the Pattern.
+         
+         
+         System.out.println("   *****");
+         System.out.println("    ***");
+         System.out.println("     *");
+           
+           
+    }
+    
+}
